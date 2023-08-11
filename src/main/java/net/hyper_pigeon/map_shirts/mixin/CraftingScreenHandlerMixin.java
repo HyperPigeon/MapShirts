07a29876061fbf9181e16abea85ca00face18286
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2023-08-11 19:42:44 -0400

    Fixed crafting

--- src/main/java/net/hyper_pigeon/map_shirts/mixin/CraftingScreenHandlerMixin.java
+++ src/main/java/net/hyper_pigeon/map_shirts/mixin/CraftingScreenHandlerMixin.java
@@ -1,8 +1,10 @@
 package net.hyper_pigeon.map_shirts.mixin;
 
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.CraftingResultInventory;
 import net.minecraft.inventory.RecipeInputInventory;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.map.MapState;
@@ -22,8 +24,8 @@ public class CraftingScreenHandlerMixin {
 
     @Inject(method = "updateResult", at = @At(value = "INVOKE",target = "net/minecraft/inventory/CraftingResultInventory.setStack (ILnet/minecraft/item/ItemStack;)V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILSOFT)
     private static void updateArmorResult(ScreenHandler handler, World world, PlayerEntity player, RecipeInputInventory craftingInventory, CraftingResultInventory resultInventory, CallbackInfo ci, ServerPlayerEntity serverPlayerEntity, ItemStack itemStack){
-        if (itemStack.hasNbt() && itemStack.getNbt().contains("mapId") && itemStack.getNbt().contains("mapState")){
-            MapState mapState = FilledMapItem.getMapState(itemStack.getOrCreateNbt().getInt("mapId"),world);
+        if (itemStack.getItem() instanceof ArmorItem armorItem && armorItem.getSlotType() == EquipmentSlot.CHEST && itemStack.hasNbt() && itemStack.getNbt().contains("mapId")){
+            MapState mapState = FilledMapItem.getMapState(itemStack.getNbt().getInt("mapId"),world);
 
             NbtCompound nbtCompound = new NbtCompound();
             mapState.writeNbt(nbtCompound);
--- src/main/java/net/hyper_pigeon/map_shirts/recipe/MapArmorRecipe.java
+++ src/main/java/net/hyper_pigeon/map_shirts/recipe/MapArmorRecipe.java
@@ -8,6 +8,7 @@ import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.map.MapState;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SpecialCraftingRecipe;
@@ -82,6 +83,8 @@ public class MapArmorRecipe extends SpecialCraftingRecipe {
         if(!mapStack.equals(ItemStack.EMPTY) && !armorStack.equals(ItemStack.EMPTY)){
 
             int mapId = FilledMapItem.getMapId(mapStack);
+
+
 
             armorStack.getOrCreateNbt().putBoolean("IsMapShirt", true);
             armorStack.getOrCreateNbt().putInt("mapId",mapId);
